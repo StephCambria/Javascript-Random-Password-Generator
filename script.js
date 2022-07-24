0b45c6ed19fe6f28bbf6cd63802fb0665293d9eb
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-07-23 21:28:50 -0400

    reworked js code

--- script.js
+++ script.js
@@ -1,66 +1,94 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function randomInt(min, max) {
+    //if 'max' is not defined, assume we want range from 0 to min
+    if (!max) {
+        max = min
+        min = 0
+    }
 
-  passwordText.value = password;
-
+    //for random value
+    var rand = Math.random()
+    return Math.floor(min + (1 - rand) + rand*max)
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-//step 1 - click button to generate password
-//step 2 - window prompt for password criteria
-//step 3 - select criteria
-//step 4 - window prompt for length of password
-//step 5 - user chooses length of 8 - 128 characters
-//step 6 - window prompt for character types to include
-//step 7 - user confirms whether or not to include lowercase, uppercase, numeric, and / or special characters
-//step 8 - when user answers each prompt, their input should be validated and at least one character type should be selected
-//step 9 - when all prompts are answered, a password is generated that matches the selected criteria
-//step 10 - when the password is generated, it is either displayed in an alert or written to the page
-
-
-
-//add functions
-
-//Random lowercase letters
-function generatePassword(){
-    getRandomLower();
-    
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+function getRandomItem(list) {
+    return list[randomInt(list.length)]
 }
-//math.random generates a random number (tied to letters of the alphabet), and math.floor returns the values to a specified range (in this case 26, because there are 26 letters in the alphabet)
 
-//Random uppercase letters
-function generatePassword(){
-    getRandomUpper();
-    
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-//same syntax, same idea
+var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var symbolList = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "?"]
+var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var optionsCart = []
+
+
+
+//Here I'm adding the generate password function
+function generatePassword() {
+    var userInput = window.prompt("How many characters do you want your password to be?")
+    var passwordLength = parseInt(userInput)
+
+    if (isNaN(passwordLength)) {
+        window.alert("Please specify a unit between 8 and 128")
+        return
+    }
+
+    if (passwordLength < 8 || passwordLength > 128) {
+        window.alert("Password must be between 8 and 128 characters")
+        return
+    }
 
 //Random numbers
-function generatePassword(){
-    getRandomNumber();
-    
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-//48 through 57 are numbers 0 through 9 in the browser character set, so we need a string of 10 for our range
+var userWantsNumbers = window.confirm("Would you like to include numbers in your password?")
+    if (userWantsNumbers === true) {
+     optionsCart.push(numberList)
+    }
+
+
+//Random lowercase letters
+var userWantsLowercase = window.confirm("Would you like to include lowercase letters in your password?")
+    if (userWantsLowercase === true) {
+    optionsCart.push(lowercaseList)
+    }
+
+
+//Random uppercase letters
+var userWantsUppercase = window.confirm("Would you like to include uppercase letters in your password?")
+    if (userWantsUppercase === true) {
+        optionsCart.push(uppercaseList)
+    }
+
+
 
 //Random symbol
-var symbols = '!@#$%^&*(){}[]=<>/,.';
-function generatePassword(){
-    getRandomSymbol();
-    return symbols[Math.floor(Math.random() * symbols.length)];
-}
-//declare the symbols variable string, then use similar math syntax to generate a random one. Instead of using numbers, multiplying by symbols.length will allow access to the entire list of symbols
+var userWantsSymbols = window.confirm("Would you like to include symbols in your password?")
+    if (userWantsSymbols === true) {
+        optionsCart.push(symbolList)
+    }
 
+
+    var generatedPassword = ""
+
+    for (var i = 0; i < passwordLength; i++) {
+        var randomList = getRandomItem(optionsCart)
+        var randomChar = getRandomItem(randomList)
+    }
+
+}
+
+
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
